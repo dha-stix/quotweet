5998563b98170a9fbfef5b0421b1e24734f0ea32
dha-stix <asaoludavid234@yahoo.com>
2022-04-25 14:19:25 +0100

    UI Edited

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
-import Pagination from "./components/Pagination";
-import Quotes from "./components/Quotes";
-import Header from "./components/Header";
-import {useState, useEffect} from "react"
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider} from '@mui/material/styles';
+import React, {lazy, Suspense} from "react"
+import {Route, Routes} from "react-router-dom"
+import {ViewInvoice} from "./pages/ViewInvoice"
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Loading from './components/Loading';
+import PageNotFound from './pages/PageNotFound';
+
+const Auth = lazy(()=> import('./pages/Auth'))
+const CreateInvoice = lazy(()=> import('./pages/CreateInvoice'))
+const Home = lazy(()=> import('./pages/Home'))
+const Dashboard = lazy(()=> import('./pages/Dashboard'))
+const SetupProfile = lazy(()=> import('./pages/SetupProfile'))
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Sora',
+      'sans-serif',
+    ].join(','),
+  },});
 
 function App() {
-  const [quotes, setQuotes ] = useState([]) 
-  const [totalPages, setTotalPages] = useState(null)
-  const [loading, setLoading] = useState(false)
-
-  const fetchQuoteTexts = () => {
-    fetch("https://api.quotable.io/quotes").then(data => data.json()).then(res => {
-      setTotalPages(res.totalPages)
-      setQuotes(res.results)
-    })
-  }
-  useEffect(()=> {
-    fetchQuoteTexts()
-  }, [])
   return (
-    <div className="w-full min-h-screen">
-      <Header/>
-      {loading ? <p>Loading</p> : <Quotes quotes={quotes}/>}
-      
-      <Pagination totalPages={totalPages} setQuotes={setQuotes} loading={loading} setLoading={setLoading}/>
-      
+    <div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Suspense fallback={<Loading/>}>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/login" element={<Auth/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/new/invoice" element={<CreateInvoice/>}/>
+        <Route path="/view/invoice/:id" element={<ViewInvoice/>}/>
+        <Route path="/profile" element={<SetupProfile/>}/>
+        <Route path="*" element={<PageNotFound/>}/>
+        
+      </Routes>
+      </Suspense>
+        <ToastContainer />
+       
+      </ThemeProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from "react-router-dom"
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+<Router>
+<Provider store={store}>
+<App />
+</Provider>
+</Router>
+)
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router>
+//     <Provider store={store}>
+//     <App />
+//     </Provider>
+//     </Router>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
